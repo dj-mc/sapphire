2a7ad0f11db61931440dd0a79f83efe2ed2e9df7
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-10-14 13:51:43 -0500

    [beryl] Implement regex tokenizer

--- beryl.py
+++ beryl.py
@@ -12,34 +12,43 @@ class Token:
     value: str
 
 
+@dataclass
+class PyInt(Token):
+    value: int
+
+
 class Tokenizer:
     def __init__(self, src_code=""):
         self.__TOKEN_TYPES = [
+            ["wsp", r"\s"],
             ["def", r"\bdef\b"],
             ["end", r"\bend\b"],
             ["var", r"\b[a-zA-Z]\b"],
             ["int", r"\b[0-9]+\b"],
-            ["lpa", r"("],
-            ["rpa", r")"],
+            ["lpa", r"\("],
+            ["rpa", r"\)"],
         ]
         self.__src_code = src_code
 
     def tokenize(self):
-        # while self.__src_code != "":
-        for token in self.__TOKEN_TYPES:
-            # symbol = token[0]
-            rgx = token[1]
-            interpolated_regex = re.compile(rf"\A{rgx}")
-            test_match = interpolated_regex.match(self.__src_code)
-            if test_match:
-                value = test_match.group()
-                print("value: " + value)
-                truncated = self.__src_code[len(value) :]
-                print("truncated: " + truncated)
-                # self.__src_code = truncated
-                return
+        tokens = []
+        while self.__src_code != "":
+            for token in self.__TOKEN_TYPES:
+                new_token = None
+                interpolated_regex = re.compile(rf"\A{token[1]}")
+                test_match = interpolated_regex.match(self.__src_code)
+                if test_match:
+                    value = test_match.group()
+                    if token[0] == "int":
+                        new_token = PyInt(token[0], value)
+                    else:
+                        new_token = Token(token[0], value)
+                    tokens.append(new_token)
+                    print(f"{value} == {token[0]}, matched regex {token[1]}")
+                    self.__src_code = self.__src_code[len(value) :]
+        return tokens
 
 
-tokens = Tokenizer("def () 1 end")
-print(tokens.__dict__["_Tokenizer__src_code"])
-tokens.tokenize()
+TokenizedObject = Tokenizer("def () 1 end")
+print(TokenizedObject.__dict__["_Tokenizer__src_code"])
+print("Captured tokens: ", TokenizedObject.tokenize())
